1017109367e8022d6ac64c5feaa1cf7bd724be75
Violeta Macsim <vmmv10@yahoo.com>
2024-11-27 17:43:44 +0100

    Add skeleton

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { Button, Checkbox, List, ListItem, ListItemText } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import wattpadLogo from './resources/Wattpad-logo.svg';
 
 const Instructions = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  function handleCheckboxChange() {
+    setIsChecked(prevValue => !prevValue)
+  }
+
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsAnimating(true);
+  };
+  
+  const customTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#004F59', // Use your custom color here
+        contrastText: '#FFFFFF', // Optional: Set text color for better contrast
+      },
+    },
+  });
+
   return (
-    <div>
-      <h1 className='bg-red-400'>Instructions</h1>
+    <div className="relative h-screen flex items-center">
+    <div className={`bg-vibrantOrange px-[30%] h-screen w-screen flex justify-center`}>
+      <ThemeProvider theme={customTheme}>
+        <div></div>
+        <div
+          className={`bg-warmWhite transform transition-transform duration-500 ${
+            isAnimating ? 'translate-x-[75%]' : 'translate-x-0'
+          }`}
+        >
+          <img src={wattpadLogo} alt="Wattpad Logo" />
+
+          <div className="justify-items-center w-full overflow-y-auto">
+            <p className="text-vibrantOrange font-bold text-[45px] text-center">
+              Welcome to the Wattpad User Feedback Task!
+            </p>
+            <div className="px-20 pt-10 text-charcoalGrey">
+              <p>
+                This study is aimed on gaining insight on what areas of our application's chapter reading page can be
+                improved for. During this task, we will only focus on the desktop view. By completing this, your
+                insight will help make Wattpad more user-friendly and enjoyable for both readers and writers.
+              </p>
+
+              <List>
+                <ListItem>
+                  <ListItemText
+                    primary="1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemText
+                    primary="2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  />
+                </ListItem>
+              </List>
+
+              <div className="flex pt-20 pb-10">
+                <Checkbox
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                  inputProps={{ 'aria-label': 'controlled' }}
+                />
+                <div className="text-[12px]">
+                  I have read and understood the instructions provided above, and I acknowledge the work I am expected
+                  to complete.
+                </div>
+              </div>
+
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={!isChecked}
+                onClick={handleButtonClick}
+              >
+                Start task
+              </Button>
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
     </div>
+  </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vibrantOrange: '#FF5000',
+        deepTeal: '#004F59',
+        mutedBlue: '#0074A2',
+        warmWhite: '#FFF6F1',
+        lightGray: '#E5E5E5',
+        charcoalGrey: '#2C2C2C',
+        goldenYellow: '#FFA834',
+        coralPink: '#FF745A'
+      }
+    },
   },
   plugins: [],
 };
