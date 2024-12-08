add45f1ca1f945ed7e58767091f07504a585acd1
ZainMunir <zainmunir999@gmail.com>
2024-12-08 17:11:43 +0100

    fix: modal+menu behind body, broken: sticky header

--- src/Clone/Body.tsx
+++ src/Clone/Body.tsx
@@ -10,7 +10,7 @@ const Body = ({ data }: Props) => {
   return (
     <div className="flex w-full flex-grow flex-col overflow-y-auto">
       <div>
-        <div className="relative h-[340px] w-full overflow-hidden">
+        <div className=" h-[340px] w-full overflow-hidden">
           <div
             style={
               // https://stackoverflow.com/questions/70805041/background-image-in-tailwindcss-using-dynamic-url-react-js
@@ -18,10 +18,10 @@ const Body = ({ data }: Props) => {
                 '--image-url': `url(${data.image})`,
               } as React.CSSProperties
             }
-            className={`absolute h-full w-full scale-110 bg-[image:var(--image-url)] bg-cover bg-center bg-no-repeat blur-xl`}
+            className={`h-full w-full scale-110 bg-[image:var(--image-url)] bg-cover bg-center bg-no-repeat blur-xl`}
           ></div>
-          <div className="absolute flex h-full w-full flex-col justify-center">
-            <div className="mx-auto flex h-[245px] w-[625px] flex-row gap-3">
+          <div className=" flex h-full w-full flex-col justify-center mt-[-340px] ">
+            <div className="mx-auto flex h-[245px] w-[625px] flex-row gap-3 z-10">
               <img src={data.image} />
               <div className="flex-grow text-white">
                 <p className="text-[15px] font-semibold">YOU ARE READING</p>
