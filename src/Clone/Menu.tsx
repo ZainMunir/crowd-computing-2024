fe3959ba4e5a0bd2799b33a68bb0542cd087c1d8
ZainMunir <zainmunir999@gmail.com>
2024-12-08 21:50:38 +0100

    add: share menu

--- src/Clone/Menu.tsx
+++ src/Clone/Menu.tsx
@@ -6,7 +6,8 @@ import { FaCaretDown } from 'react-icons/fa';
 
 type MenuProps = {
   children: React.ReactNode;
-  buttonVal: string;
+  buttonVal: React.ReactNode;
+  isElement?: boolean;
 };
 
 function useOutsideClick(wrapperRef, buttonRef, onClickOutside) {
@@ -28,7 +29,7 @@ function useOutsideClick(wrapperRef, buttonRef, onClickOutside) {
   }, [wrapperRef, buttonRef, onClickOutside]);
 }
 
-const Menu = ({ children, buttonVal }: MenuProps) => {
+const Menu = ({ children, buttonVal, isElement = false }: MenuProps) => {
   const [isShown, setIsShown] = useState(false);
   const wrapperRef = useRef(null);
   const buttonRef = useRef(null);
@@ -38,16 +39,18 @@ const Menu = ({ children, buttonVal }: MenuProps) => {
     setIsShown(!isShown);
   };
 
+
   return (
     <div className="relative inline-block">
-      <button
-        onClick={handleClick}
-        className="flex h-full cursor-pointer items-center gap-1 font-semibold"
-        ref={buttonRef}
-      >
-        {buttonVal}
-        <FaCaretDown />
-      </button>
+
+        <button
+          onClick={handleClick}
+          className="flex h-full cursor-pointer items-center gap-1 font-semibold"
+          ref={buttonRef}
+        >
+          {buttonVal}
+          {!isElement &&<FaCaretDown />}
+        </button>
       {isShown && (
         <div
           className="absolute left-0 top-[90%] z-50 mt-2 border border-gray-300 bg-white drop-shadow-[0_0px_5px_rgba(0,0,0,0.25)]"
--- src/Clone/ShareButtons.tsx
+++ src/Clone/ShareButtons.tsx
@@ -5,6 +5,11 @@ import { FaPinterest } from 'react-icons/fa';
 import { FaTumblr } from 'react-icons/fa';
 import { ImEmbed2 } from 'react-icons/im';
 import { FaEllipsisH } from 'react-icons/fa';
+import { FaUserAlt } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+import { FaExclamationCircle } from 'react-icons/fa';
+import Menu from './Menu';
+import { Divider } from '@mui/material';
 
 type Props = {
   flexVal: string;
@@ -12,26 +17,46 @@ type Props = {
 
 const ShareButtons = ({ flexVal }: Props) => {
   const icons = [
-    { icon: <RiFacebookFill className='text-white '/>, color: 'bg-indigo-800' },
-    { icon: <FaTwitter className='text-white'/>, color: 'bg-blue-400' },
-    { icon: <FaPinterest className='text-white'/>, color: 'bg-red-700' },
-    { icon: <FaTumblr className='text-white'/>, color: 'bg-slate-700' },
-    { icon: <ImEmbed2 className='text-white'/>, color: 'bg-neutral-500' },
-    { icon: <FaEllipsisH className='text-neutral-500'/>, color: 'bg-zinc-100' },
+    { icon: <RiFacebookFill className="text-white" />, color: 'bg-indigo-800' },
+    { icon: <FaTwitter className="text-white" />, color: 'bg-blue-400' },
+    { icon: <FaPinterest className="text-white" />, color: 'bg-red-700' },
+    { icon: <FaTumblr className="text-white" />, color: 'bg-slate-700' },
+    { icon: <ImEmbed2 className="text-white" />, color: 'bg-neutral-500' },
+    {
+      icon: <FaEllipsisH className="text-neutral-500" />,
+      color: 'bg-zinc-100',
+    },
   ];
+
+  const circles = icons.map((icon, index) => {
+    return (
+      <div
+        key={index}
+        className={`flex h-10 w-10 items-center justify-center rounded-full ${icon.color} cursor-pointer text-2xl`}
+      >
+        {icon.icon}
+      </div>
+    );
+  });
 
   return (
     <div className={`flex ${flexVal} gap-2`}>
-      {icons.map((icon, index) => {
-        return (
-          <div
-            key={index}
-            className={`flex h-10 w-10 items-center justify-center rounded-full ${icon.color} text-2xl`}
-          >
-            {icon.icon}
+      {circles.slice(0, -1)}
+      <Menu buttonVal={circles[circles.length - 1]} isElement={true}>
+        <div className="w-44 p-2 text-neutral-500">
+          <div className="flex flex-row items-center gap-2">
+            <FaUserAlt /> Post to Your Profile
           </div>
-        );
-      })}
+          <Divider className="my-1" />
+          <div className="flex flex-row items-center gap-2">
+            <MdEmail /> Share via Email
+          </div>
+          <Divider className="my-1" />
+          <div className="flex flex-row items-center gap-2">
+            <FaExclamationCircle /> Report Story
+          </div>
+        </div>
+      </Menu>
     </div>
   );
 };
